ebd0acbaaa5f87b9caddf7ebc61d3999aa68d850
fanzhen <fanz@cpwxw.net>
2019-11-08 09:59:08 +0800

    commit1

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>hello</h1>
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style  lang="stylus" rel="stylesheet/stylus">
+ h1
+   font-size 20px
+   color red
+   
+
+</style>
